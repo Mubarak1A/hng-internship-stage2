cfa38a086d2964b3da0d28e331301da05422d972
Mubarak <desinamubarak123@gmail.com>
2024-07-07 08:55:34 +0100

    create: create user controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+//import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  //@UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    const user = await this.usersService.findOneById(id);
+    return {
+      status: 'success',
+      message: 'User retrieved successfully',
+      data: {
+        userId: user.userId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+      },
+    };
+  }
+}
