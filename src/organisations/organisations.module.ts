4b1029069bdef6af327d71c2ed369fb8804cb7ae
Mubarak <desinamubarak123@gmail.com>
2024-07-07 09:14:30 +0100

    create: create organisation module

---
+++ src/organisations/organisations.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organisation } from './organisation.entity';
+import { OrganisationsService } from './organisations.service';
+import { OrganisationsController } from './organisations.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Organisation])],
+  providers: [OrganisationsService],
+  controllers: [OrganisationsController],
+  exports: [OrganisationsService],
+})
+export class OrganisationsModule {}
