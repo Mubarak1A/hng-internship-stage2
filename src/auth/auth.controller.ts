bf8ea128d89a06a34df3af2fc443a14a6ebf6d29
Mubarak <desinamubarak123@gmail.com>
2024-07-07 09:01:55 +0100

    create: create auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
+import { OrganisationsService } from '../organisations/organisations.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    private organisationsService: OrganisationsService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(@Body() user: User) {
+    const createdUser = await this.usersService.create(user);
+    await this.organisationsService.createDefaultOrganisation(createdUser);
+    const loginResponse = await this.authService.login(createdUser);
+    return {
+      status: 'success',
+      message: 'Registration successful',
+      data: loginResponse,
+    };
+  }
+}
