303e1a76d84e3071e19fb51faaf134057b6ac0d6
Mubarak <desinamubarak123@gmail.com>
2024-07-07 09:17:51 +0100

    update: add auth guard to user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-//import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  //@UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getUser(@Param('id') id: string) {
     const user = await this.usersService.findOneById(id);
