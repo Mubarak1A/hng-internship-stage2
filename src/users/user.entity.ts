e5015274ce97a6aa6876460fe668ba52addb98e3
Mubarak <desinamubarak123@gmail.com>
2024-07-07 08:51:17 +0100

    create: create user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, Unique, ManyToMany } from 'typeorm';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+//import { Organisation } from '../organisations/organisation.entity';
+
+@Entity()
+@Unique(['userId', 'email'])
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column()
+  @IsNotEmpty()
+  firstName: string;
+
+  @Column()
+  @IsNotEmpty()
+  lastName: string;
+
+  @Column()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Column()
+  @IsNotEmpty()
+  password: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  //@ManyToMany(() => Organisation, (organisation) => organisation.users)
+  //organisations: Organisation[];
+}
